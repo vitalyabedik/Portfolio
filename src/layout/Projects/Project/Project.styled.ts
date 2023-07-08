7e08bdd3ec69c555925bd2d3a4acdbc23199b24b
Vitali Bedik <vitalya.bedik@gmail.com>
2023-07-08 11:19:58 +0300

    feat: basic styles for project

--- src/layout/Projects/Project/Project.styled.ts
+++ src/layout/Projects/Project/Project.styled.ts
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
 
 export const ProjectContainer = styled.div`
-  width: 360px;
+  display: flex;
+  //width: 700px;
+
+  background-color: #182636;
+  padding: 20px;
+  //width: 800px;
   //min-width: 40%;
   border: 1px solid;
-  background-color: #F7F7F7;
+  //background-color: #F7F7F7;
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
 `
 
 type ProjectImagePropsType = {
@@ -15,38 +24,119 @@ export const ProjectImage = styled.div<ProjectImagePropsType>`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 200px;
-  width: 100%;
-  background-color: #9e9ea2;
+  height: 300px;
+  //height: 400px;
+  width: 600px;
   background-image: url(${props => props.backgroundimage});
   background-size: cover;
   background-repeat: no-repeat;
   background-position: 50%;
+  border-radius: 10px;
+
+  @media (max-width: 1024px) {
+    width: 100%;
+    margin-bottom: 20px;
+    height: 400px;
+  }
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
+
+  @media (max-width: 576px) {
+    height: 250px;
+  }
 `
 
-export const ProjectLink = styled.a`
-  background-color: #666667;
-  padding: 15px 25px;
-  cursor: pointer;
+export const ProjectHeader = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  //flex-direction: column;
+`
+
+export const ProjectMainInfo = styled.div`
+  display: flex;
+  width: 100%;
+  //flex-direction: column;
 `
 
 export const ProjectInfo = styled.div`
-  background-color: #d1d1d1;
+  //background-color: #d1d1d1;
+  width: 100%;
   text-align: left;
-  padding: 10px 20px;
+  padding-left: 20px;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
 
-`
-
-export const ProjectTitle = styled.h3`
-  margin-bottom: 10px;
-  background-color: #9e9ea2;
+  @media (max-width: 1024px) {
+    padding-left: 0px;
+  }
 `
 
 export const ProjectDescription = styled.div`
-  background-color: #9e9ea2;
-  width: 100%;
-  padding-bottom: 30px;
+  //background-color: #9e9ea2;
+  //width: 100%;
+  margin-top: 10px;
+  //margin-bottom: 30px;
+
+  font-size: 1.6rem;
+  line-height: 2.4rem;
+  color: #BBBBBB
 `
+export const ProjectStack = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 20px;
+`
+
+export const ProjectStackItem = styled.span`
+  //font-size: 1.6rem;
+  //padding: 10px;
+  ////margin: 0 10px;
+  //text-align: center;
+  //
+  //color: #4bffa5;
+  //line-height: 2.4rem;
+  //font-weight: 600;
+  //
+  //border: 1px solid white;
+  //border-radius: 30px;
+
+  font-size: 1.6rem;
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  //background-color: #fff;
+  color: #4bffa5;
+  border-radius: 30px;
+  margin-right: 5px;
+  padding: 5px 10px;
+  padding-left: 0;
+  white-space: nowrap;
+`
+export const ProjectLinks = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
+`
+
+
+export const ProjectLink = styled.a`
+  margin-top: 20px;
+  padding-right: 15px;
+
+  svg {
+    transition: fill 0.3s ease-in-out, transform 0.3s ease-in-out;
+  }
+
+  &:hover svg {
+    fill: #4bffa5;
+    transform: scale(1.05);
+  }
+`;
--- src/layout/Projects/Project/Project.tsx
+++ src/layout/Projects/Project/Project.tsx
@@ -1,30 +1,60 @@
 import React from 'react';
 
 import {
-    ProjectContainer,
-    ProjectDescription,
-    ProjectImage,
-    ProjectInfo,
-    ProjectLink,
-    ProjectTitle
+    ProjectContainer, ProjectDescription, ProjectHeader,
+    ProjectImage, ProjectInfo, ProjectLink, ProjectLinks, ProjectMainInfo,
+    ProjectStack, ProjectStackItem,
 } from './Project.styled';
 
-type PropsType = {
-    image: string
-    title: string
-    description: string
-}
+import {Badge, Title3} from '../../../components';
+import {ProjectType} from '../../../data';
+import {Icon} from '../../../components/Icon';
+import {RiGithubFill} from 'react-icons/ri';
+import {BiLinkExternal} from 'react-icons/bi';
 
-export const Project: React.FC<PropsType> = ({image, title, description}) => {
+
+export const Project: React.FC<ProjectType> = (props) => {
+    const {
+        title,
+        demoUrl,
+        githubUrl,
+        image,
+        description,
+        technologies,
+        badge
+    } = props
+
     return (
         <ProjectContainer>
             <ProjectImage backgroundimage={image}>
                 <ProjectLink>See project</ProjectLink>
             </ProjectImage>
-            <ProjectInfo>
-                <ProjectTitle>{title}</ProjectTitle>
-                <ProjectDescription>{description}</ProjectDescription>
-            </ProjectInfo>
+            <ProjectMainInfo>
+                <ProjectInfo>
+                    <ProjectHeader>
+                        <Title3>{title}</Title3>
+                        <Badge>{badge}</Badge>
+                    </ProjectHeader>
+                    <ProjectStack>
+                        {
+                            technologies.map(technology => {
+                                return (
+                                    <ProjectStackItem>{technology}</ProjectStackItem>
+                                )
+                            })
+                        }
+                    </ProjectStack>
+                    <ProjectDescription>{description}</ProjectDescription>
+                    <ProjectLinks>
+                        <ProjectLink href={demoUrl} target='_blank'>
+                            <Icon icon={BiLinkExternal} size={'24px'} color={'white'}/>
+                        </ProjectLink>
+                        <ProjectLink href={githubUrl} target='_blank'>
+                            <Icon icon={RiGithubFill} size={'24px'} color={'white'}/>
+                        </ProjectLink>
+                    </ProjectLinks>
+                </ProjectInfo>
+            </ProjectMainInfo>
         </ProjectContainer>
     );
 };
